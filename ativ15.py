cde332c82c1b191043603ecee73185a6bcf62d69
olga-passos39 <olgapassos457@gmail.com>
2024-09-16 09:05:15 -0300

    operaçao

---
+++ ativ15.py
@@ -0,0 +1,17 @@
+# Crie um programa que receba dois números e uma operação (soma, subtração, multiplicação ou divisão) e execute a operação desejada.
+
+a = float(input("Primeiro número:"))
+b = float(input("Segundo número:"))
+operação = input("Digite a operação a realizar (+,-,* ou /):")
+if operação == "+":
+    resultado = a + b
+elif operação == "-":
+    resultado = a - b
+elif operação == "*":
+    resultado = a * b
+elif operação == "/":
+    resultado = a / b
+else:
+    print("Operação inválida!")
+    resultado = 0
+print("Resultado: ", resultado)
